0d0f85357ead10b1e7099001006da267fe81b656
Dunas-Lopiin <plinio.figueiredo.s@gmail.com>
2022-04-26 15:01:20 -0300

    Adicionada a rota de visualização de usuário

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,23 @@
 // config
-import express from 'express';
+import express from "express";
+const app = express();
+const port = 3001;
+import "dotenv/config";
 
 // middlewares
 
 // routes
-import auth from './routes/auth';
-import register from './routes/register';
-import edit from './routes/edit';
-import deleteUser from './routes/delete';
+//import auth from "./routes/auth";
+//import register from "./routes/register";
+//import edit from "./routes/edit";
+//import deleteUser from "./routes/delete";
+import view from "./routes/view.js";
+const viewRoute = view;
 
-const app = express();
-const port = 3001;
-
-app.use('/auth', auth);
-app.use('/register', register);
-app.use('/edit', edit);
-app.use('/deleteUser', deleteUser);
+/* app.use("/auth", auth);
+app.use("/register", register);
+app.use("/edit", edit);
+app.use("/deleteUser", deleteUser); */
+app.use("/view", viewRoute);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
