decd9711c8fb387951fda0617c469b5b60ce7ed2
Felype Nascimento <51304121+FelypeNasc@users.noreply.github.com>
2022-04-28 00:01:03 -0300

    feat: little setup in home page

--- frontend/src/pages/HomePage/index.jsx
+++ frontend/src/pages/HomePage/index.jsx
@@ -1,18 +1,26 @@
-import {
-  PositiveButton,
-  NegativeButton,
-} from "../../components/exporterOfComponents";
-
+import { NavLink } from "react-router-dom";
+import { useState } from "react";
 import "./style.css";
 
-function Home() {
+function HomePage() {
   return (
-    <div>
-      <h1>Home page</h1>
-      <NegativeButton path="/" label="Sair" />
-      <PositiveButton path="/edit-profile" label="Editar perfil" />
+    <div id="home-page">
+      <h1>Home Page</h1>
+      <div id="profile-pic"></div>
+      <div id="profile-info">
+        <h2 id="username">FelypeNasc</h2>
+        <p id="email">felype.nasc@hotmail.com</p>
+        <p id="birthdate">Birthdate: 19/02/1996</p>
+      </div>
+      <div className="navigate-buttons">
+        <button id="logout-button">Logout</button>
+
+        <button id="edit-button">
+          <NavLink to="/edit">Edit</NavLink>
+        </button>
+      </div>
     </div>
   );
 }
 
-export default Home;
+export default HomePage;
