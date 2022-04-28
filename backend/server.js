d0be7736304375b7b8878ec957824d57f071c5ab
Dunas-Lopiin <plinio.figueiredo.s@gmail.com>
2022-04-28 14:17:17 -0300

    mudanças

--- backend/server.js
+++ backend/server.js
@@ -7,17 +7,17 @@ import "dotenv/config";
 // middlewares
 
 // routes
-//import auth from "./routes/auth";
-//import register from "./routes/register";
-//import edit from "./routes/edit";
-//import deleteUser from "./routes/delete";
+import auth from "./routes/auth";
+import register from "./routes/register";
+import edit from "./routes/edit";
+import deleteUser from "./routes/delete";
 import view from "./routes/view.js";
 const viewRoute = view;
 
-/* app.use("/auth", auth);
+app.use("/auth", auth);
 app.use("/register", register);
 app.use("/edit", edit);
-app.use("/deleteUser", deleteUser); */
+app.use("/deleteUser", deleteUser);
 app.use("/view", viewRoute);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
