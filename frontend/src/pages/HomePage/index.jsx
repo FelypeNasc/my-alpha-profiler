8f1cc2256cc444be0a076a94c91d67456d4bc8b6
Felype Nascimento <51304121+FelypeNasc@users.noreply.github.com>
2022-04-28 02:03:43 -0300

    feat: basic home page styling and structure

--- frontend/src/pages/HomePage/index.jsx
+++ frontend/src/pages/HomePage/index.jsx
@@ -2,23 +2,23 @@ import { NavLink } from "react-router-dom";
 import { useState } from "react";
 import "./style.css";
 
+import StandardHeader from "../../components/headers/StandardHeader";
+
 function HomePage() {
   return (
-    <div id="home-page">
-      <h1>Home Page</h1>
-      <div id="profile-pic"></div>
-      <div id="profile-info">
-        <h2 id="username">FelypeNasc</h2>
-        <p id="email">felype.nasc@hotmail.com</p>
-        <p id="birthdate">Birthdate: 19/02/1996</p>
-      </div>
-      <div className="navigate-buttons">
-        <button id="logout-button">Logout</button>
-
-        <button id="edit-button">
-          <NavLink to="/edit">Edit</NavLink>
-        </button>
-      </div>
+    <div className="page" id="home-page">
+      <StandardHeader />
+      <main id="home-page-main">
+        <div id="profile-picture"></div>
+        <div id="profile-info">
+          <h2 id="username">FelypeNasc</h2>
+          <p id="email">felype.nasc@hotmail.com</p>
+          <p id="birthdate">Birthdate: 19/02/1996</p>
+        </div>
+        <div className="navigate-buttons">
+          <button id="edit-button">Edit Profile</button>
+        </div>
+      </main>
     </div>
   );
 }
