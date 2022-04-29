5f23cbae8e283a79be46d5c3e7601f7a1af2c09e
Felype Nascimento <51304121+FelypeNasc@users.noreply.github.com>
2022-04-29 12:09:25 -0300

    refactor: change header component

--- frontend/src/components/headers/StandardHeader/index.jsx
+++ frontend/src/components/headers/StandardHeader/index.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import LogoutButton from "../../buttons/LogoutButton";
 import "./style.css";
 
-const StandardHeader = () => {
+const StandardHeader = ({ children }) => {
   return (
     <header id="standard-header">
       <NavLink to="/">
         <p id="profiler-header">Profiler</p>
       </NavLink>
-      <LogoutButton />
+      {children}
     </header>
   );
 };
