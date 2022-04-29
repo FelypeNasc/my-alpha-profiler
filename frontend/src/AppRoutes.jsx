5857a7346dbbc8fd27534657f71a8d67fcdd1307
Felype Nascimento <51304121+FelypeNasc@users.noreply.github.com>
2022-04-29 12:04:15 -0300

    feat: private routes for logged and not

--- frontend/src/AppRoutes.jsx
+++ frontend/src/AppRoutes.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import EditProfilePage from "./pages/EditProfilePage";
-import SignUpPage from "./pages/SignUpPage";
+import RegisterPage from "./pages/RegisterPage";
 
 import { AuthProvider, AuthContext } from "./context/auth";
 
@@ -19,16 +19,64 @@ const AppRoutes = () => {
     if (!authenticated) {
       return <Navigate to={"/login"} />;
     }
+
+    return children;
+  };
+
+  const LoginPrivate = ({ children }) => {
+    const { authenticated, isLoading } = useContext(AuthContext);
+
+    if (isLoading) {
+      return <div className="loading">Loading...</div>;
+    }
+
+    if (authenticated) {
+      return <Navigate to={"/"} />;
+    }
+
+    return children;
   };
   return (
     <div>
       <Router>
         <AuthProvider>
           <Routes>
-            <Route exact path="/" element={<HomePage />} />
-            <Route exact path="/login" element={<LoginPage />} />
-            <Route exact path="/signup" element={<SignUpPage />} />
-            <Route exact path="/edit-profile" element={<EditProfilePage />} />
+            <Route
+              exact
+              path="/"
+              element={
+                <Private>
+                  <HomePage />
+                </Private>
+              }
+            />
+            <Route
+              exact
+              path="/login"
+              element={
+                <LoginPrivate>
+                  <LoginPage />
+                </LoginPrivate>
+              }
+            />
+            <Route
+              exact
+              path="/register"
+              element={
+                <LoginPrivate>
+                  <RegisterPage />
+                </LoginPrivate>
+              }
+            />
+            <Route
+              exact
+              path="/edit-profile"
+              element={
+                <Private>
+                  <EditProfilePage />
+                </Private>
+              }
+            />
           </Routes>
         </AuthProvider>
       </Router>
