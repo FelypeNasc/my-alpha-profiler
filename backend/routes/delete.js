ef09ad3380cc6466b6dee8be765c86ce7c44cbb7
Dudeful <rellumnyar@gmail.com>
2022-04-30 14:31:52 -0300

    just add 'validateToken' middleware

--- backend/routes/delete.js
+++ backend/routes/delete.js
@@ -1,32 +1,32 @@
-import { Router } from "express";
+import { Router } from 'express';
+import pg from 'pg';
+import validateToken from '../middlewares/validate-token.js';
 const router = Router();
-import pg from "pg";
 const { Client } = pg;
 
 // modules
 
 // route
-router.delete("/", async (req, res) => {
-	console.log("delete initiated");
-	async function deleteUser(user) {
-		const client = new Client();
-		try {
-			let query =
-				"UPDATE public.users SET deleted = true WHERE username=$1";
-			await client.connect();
-			console.log("conectado ao banco");
-			await client.query(query, [user]);
-			console.log(user);
-			await client.end();
-			return "end-conection";
-		} catch (e) {
-			await client.query("ROLLBACK");
-			console.log("erro:", e);
-		} finally {
-			await client.end();
-			console.log("Fim da conexão");
-		}
-	}
-	res.send(await deleteUser(req.query.user));
+router.delete('/', validateToken, async (req, res) => {
+  console.log('delete initiated');
+  async function deleteUser(user) {
+    const client = new Client();
+    try {
+      let query = 'UPDATE public.users SET deleted = true WHERE username=$1';
+      await client.connect();
+      console.log('conectado ao banco');
+      await client.query(query, [user]);
+      console.log(user);
+      await client.end();
+      return 'end-conection';
+    } catch (e) {
+      await client.query('ROLLBACK');
+      console.log('erro:', e);
+    } finally {
+      await client.end();
+      console.log('Fim da conexão');
+    }
+  }
+  res.send(await deleteUser(req.query.user));
 });
 export default router;
