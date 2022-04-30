d8a9b5b912bc15ea6bb2b83eef1852311a782072
Felype Nascimento <51304121+FelypeNasc@users.noreply.github.com>
2022-04-30 15:33:51 -0300

    feat-temp: apply registerContext to register page

--- frontend/src/pages/HomePage/index.jsx
+++ frontend/src/pages/HomePage/index.jsx
@@ -1,25 +1,42 @@
-import { NavLink } from "react-router-dom";
-import { useState } from "react";
 import "./style.css";
+import { useNavigate } from "react-router-dom";
 
 import StandardHeader from "../../components/headers/StandardHeader";
 import LogoutButton from "../../components/buttons/LogoutButton";
+import { AuthContext } from "../../context/auth";
+import { useContext } from "react";
 
 function HomePage() {
+  const navigate = useNavigate();
+  const { user } = useContext(AuthContext);
+  const { username, email, birthday, picURL } = user;
+
+  const handleEditProfile = () => {
+    navigate("/edit-profile");
+  };
+
   return (
     <div className="page" id="home-page">
       <StandardHeader>
         <LogoutButton />
       </StandardHeader>
       <main id="home-page-main">
-        <div id="profile-picture"></div>
+        <div id="profile-picture">
+          {picURL ? (
+            <img src={picURL} alt="Profile" />
+          ) : (
+            <img src="https://t4.ftcdn.net/jpg/03/31/69/91/360_F_331699188_lRpvqxO5QRtwOM05gR50ImaaJgBx68vi.jpg" alt="Profile" />
+          )}
+        </div>
         <div id="profile-info">
-          <h2 id="username">Pedro Cardoso</h2>
-          <p id="email">pedrão_brabo@email.com</p>
-          <p id="birthdate">Birthdate: 01/01/1900</p>
+          <h2 id="username">{username}</h2>
+          <p id="email">Email: {email}</p>
+          <p id="birthdate">Birthdate: {birthday}</p>
         </div>
         <div className="navigate-buttons">
-          <button id="edit-button">Edit Profile</button>
+          <button id="edit-button" onClick={handleEditProfile}>
+            Edit Profile
+          </button>
         </div>
       </main>
     </div>
--- frontend/src/pages/RegisterPage/index.jsx
+++ frontend/src/pages/RegisterPage/index.jsx
@@ -1,76 +1,89 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { RegisterContext } from "../../context/register";
 
 import StandardHeader from "../../components/headers/StandardHeader";
-
 import "./style.css";
 
 function RegisterPage() {
-  const [fullname, setFullname] = useState("");
+  console.log(useContext(RegisterContext))
+  const { register } = useContext(RegisterContext);
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [birthday, setBirthday] = useState("");
-
+  const [error, setError] = useState("");
   const navigate = useNavigate();
-  
-  function handleSubmit(event) {
+
+  function handleSubmitRegister(event) {
     event.preventDefault();
+
+    if (password !== confirmPassword) {
+      setError("Passwords do not match");
+    } else {
+      // register(username, password, email, birthday);
+
+      setUsername("");
+      setEmail("");
+      setPassword("");
+      setConfirmPassword("");
+      setBirthday("");
+
+      navigate("/");
+
+      setError("");
+    }
   }
   return (
     <div className="page" id="register-page">
       <StandardHeader />
-      <main>
-        <h2>Register</h2>
-        <form id="register-form">
-          <div className="register-input-container">
-            <input
-              className="reg-input"
-              type="text"
-              placeholder="Full Name"
-              onChange={(e) => setFullname(e.target.value)}
-            />
-            <input
-              className="reg-input"
-              type="text"
-              placeholder="Username"
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <input
-              className="reg-input"
-              type="password"
-              placeholder="Password"
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <input
-              className="reg-input"
-              type="password"
-              placeholder="Confirm Password"
-              onChange={(e) => setConfirmPassword(e.target.value)}
-            />
-            <input
-              className="reg-input"
-              type="email"
-              placeholder="Email"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <input
-              className="reg-input"
-              type="text"
-              id="reg-birthday"
-              placeholder="Birthday"
-              onChange={(e) => setBirthday(e.target.value)}
-              onFocus={(e) => (e.target.type = "date")}
-              onBlur={(e) => (e.target.type = "text")}
-            />
-          </div>
-          <div className="button-container">
-            <button id="send">Register</button>
-          </div>
-          <p></p>
-        </form>
-      </main>
+        <main>
+          <h2>Register</h2>
+
+          <form id="register-form">
+            <div className="register-input-container">
+              <input
+                className="reg-input"
+                type="text"
+                placeholder="Username"
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <input
+                className="reg-input"
+                type="email"
+                placeholder="Email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                className="reg-input"
+                type="password"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <input
+                className="reg-input"
+                type="password"
+                placeholder="Confirm Password"
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+              <input
+                className="reg-input"
+                type="text"
+                id="reg-birthday"
+                placeholder="Birthday"
+                onChange={(e) => console.log(e.target.value)}
+                onFocus={(e) => (e.target.type = "date")}
+                onBlur={(e) => (e.target.type = "text")}
+              />
+            </div>
+            <div className="button-container">
+              <button id="send">Register</button>
+            </div>
+            <div className="error-container">{error && <p className="error">{error}</p>}</div>
+            <p></p>
+          </form>
+        </main>
     </div>
   );
 }
