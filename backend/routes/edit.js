3c9b7a4640b9e880ef14663cdbe6f8de1082c421
Dudeful <rellumnyar@gmail.com>
2022-04-30 18:03:20 -0300

    integrate plinio's code with danubio's code

--- backend/routes/edit.js
+++ backend/routes/edit.js
@@ -1,38 +1,43 @@
-import { Router } from "express";
+import { Router } from 'express';
+import pg from 'pg';
+import validateToken from '../middlewares/validate-token.js';
+import bcrypt from 'bcrypt';
+const saltRounds = 12;
 const router = Router();
-import pg from "pg";
 const { Client } = pg;
 
-// modules
-/* 		const queryInsert = `UPDATE public.users SET password=$1, email=$2, photo=$3 WHERE username=$4; 
-		`; */
-// route
+router.put('/', validateToken, async (req, res) => {
+  const client = new Client();
+  const { data } = req.body;
 
-router.put("/", async (req, res) => {
-	const client = new Client();
-	console.log(req.body);
-	let queryUser = req.body.username;
-	let queryEmail = req.body.email;
-	let queryPass = req.body.password;
-	let queryPhoto = req.body.photo;
-	/* 	const queryInsert = `UPDATE public.users SET ${queryPass} ${queryEmail} ${queryPhoto} WHERE ${queryUser};`;
-	 */
-	//res.send("ok");
+  if (data.password) {
+    const hash = bcrypt.hashSync(data.password, saltRounds);
+    data.password = hash;
+  }
 
-	try {
-		await client.connect();
-		const queryInsert = `UPDATE public.users SET password=$1, email=$2, photo=$3 WHERE username=$4;`;
+  const updates = [];
+  Object.entries(data).forEach((entry, index, array) => {
+    if (index === array.length - 1) {
+      updates.push(` ${entry[0]}='${entry[1]}' `);
+    } else {
+      updates.push(` ${entry[0]}='${entry[1]}'`);
+    }
+  });
 
-		const values = [queryPass, queryEmail, queryPhoto, queryUser];
-		await client.query(queryInsert, values);
-	} catch (error) {
-		console.error(error);
-		await client.query("ROLLBACK");
-		res.send("Problemas ocorreram, Rollback realizado");
-	} finally {
-		res.send("Dados atualizados");
-		await client.end();
-	}
+  try {
+    await client.connect();
+    const queryInsert = `UPDATE public.users SET ${updates.join()} WHERE username=$1;`;
+    const values = [data.username];
+
+    await client.query(queryInsert, values);
+  } catch (error) {
+    console.error(error);
+    await client.query('ROLLBACK');
+    res.send('Problemas ocorreram, Rollback realizado');
+  } finally {
+    res.send('Dados atualizados');
+    await client.end();
+  }
 });
 
 export default router;
