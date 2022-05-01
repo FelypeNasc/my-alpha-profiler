6574805997532a077873fba2c026d519ebae4e3d
Jo-Neto <jcb.oliveira.neto@gmail.com>
2022-05-01 19:06:11 -0300

    delete route done

--- backend/routes/delete.js
+++ backend/routes/delete.js
@@ -1,30 +1,52 @@
 import { Router } from 'express';
 import pg from 'pg';
 import validateToken from '../middlewares/validate-token.js';
+
 const router = Router();
 const { Client } = pg;
 
 router.delete('/', validateToken, async (req, res) => {
-  res.send(req.user);
-  // console.log('delete initiated');
-  // async function deleteUser(user) {
-  //   const client = new Client();
-  //   try {
-  //     let query = 'UPDATE public.users SET deleted = true WHERE username=$1';
-  //     await client.connect();
-  //     console.log('conectado ao banco');
-  //     await client.query(query, [user]);
-  //     console.log(user);
-  //     await client.end();
-  //     return 'end-conection';
-  //   } catch (e) {
-  //     await client.query('ROLLBACK');
-  //     console.log('erro:', e);
-  //   } finally {
-  //     await client.end();
-  //     console.log('Fim da conexão');
-  //   }
-  // }
-  // res.send(await deleteUser(req.query.user));
+  
+  console.log('DELETE ROUTE --> initiated');
+ 
+  //-----------------------||-----------------------||-----------------------
+  async function deleteUser(user) { 
+    
+    const client = new Client();
+    
+    try {
+      if (user.isAuth) {
+        let query = 'UPDATE public.users SET deleted = true WHERE username=$1';
+        await client.connect();
+        console.log('DELETE ROUTE --> connected do database');
+        await client.query(query, [user.username]);
+        await client.end();
+        res.status(200).send('delete-success');
+        console.log('DELETE ROUTE --> deleted username: '+ user.username);
+      } else {
+        res.status(401).send('delete-failure');
+        console.log('DELETE ROUTE --> wrong acc/pwd on request');
+      }
+    } 
+    
+    catch (e) {
+      await client.query('ROLLBACK');
+      console.log('DELETE ROUTE --> error: ');
+      console.log(e);
+      await client.end();
+      res.status(500).send('delete-failure');
+    } 
+    
+    finally {
+      console.log('DELETE ROUTE --> Fim da conexão');
+    }
+
+  }
+  //-----------------------||-----------------------||-----------------------
+  
+  await deleteUser(req.user);
+  console.log('DELETE ROUTE --> finishing call');
+
 });
+
 export default router;
