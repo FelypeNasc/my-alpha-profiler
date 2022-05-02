150212f443e1334fd58896e52df3b4d87aa339ee
Felype Nascimento <felype.nasc@hotmail.com>
2022-05-01 22:02:12 -0300

    fix: now error showing right

--- frontend/src/pages/EditProfilePage/index.jsx
+++ frontend/src/pages/EditProfilePage/index.jsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { EditContext } from '../../context/edit';
 
 import StandardHeader from '../../components/headers/StandardHeader';
 import LogoutButton from '../../components/buttons/LogoutButton';
@@ -7,6 +8,7 @@ import validateEditRequest from '../../modules/validateEditRequest';
 import './style.css';
 
 function EditProfilePage() {
+  const { deleteAccount, editProfile, error } = useContext(EditContext);
   const defaultPhoto =
     'https://t4.ftcdn.net/jpg/03/31/69/91/360_F_331699188_lRpvqxO5QRtwOM05gR50ImaaJgBx68vi.jpg';
   const [photo, setPhoto] = useState('');
@@ -14,7 +16,6 @@ function EditProfilePage() {
   const [confirmEmail, setConfirmEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
-  const [error, setError] = useState('');
   const navigate = useNavigate();
   const handleChangeImage = (e) => {
     const reader = new FileReader();
@@ -24,33 +25,11 @@ function EditProfilePage() {
     reader.readAsDataURL(e.target.files[0]);
     setPhoto(e.target.files[0]);
   };
-  const handleSubmit = async (e) => {
+  const handleSubmitChanges = (e) => {
     e.preventDefault();
-    if (email !== confirmEmail) {
-      setError('Emails do not match');
-    } else if (password !== confirmPassword) {
-      setError('Passwords do not match');
-    } else {
-      try {
-        const requestBody = validateEditRequest(photo, email, password);
-        const api = 'http://localhost:3001/';
-        const response = await fetch(`${api}edit`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(),
-        });
-        const data = await response.json();
-        console.log(data);
-        navigate('/');
-      } catch (error) {
-        setError(error.message);
-      }
-
-      setError('');
-      navigate('/home');
-    }
+  };
+  const handleSubmitDelete = (e) => {
+    e.preventDefault();
   };
   return (
     <div className="page" id="edit-profile-page">
@@ -94,8 +73,12 @@ function EditProfilePage() {
             />
           </div>
           <div className="edit-profile-button-container">
-            <button id="delete-account-button">Delete my account</button>
-            <button id="send">Save Changes</button>
+            <button id="delete-account-button" onClick={handleSubmitDelete}>
+              Delete my account
+            </button>
+            <button id="send" onClick={handleSubmitChanges}>
+              Save Changes
+            </button>
           </div>
           <div className="error-container">{error && <p className="error">{error}</p>}</div>
           <p></p>
