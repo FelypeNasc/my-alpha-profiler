c60f3a40f47f888482c50746f38841b9cf574043
Felype Nascimento <felype.nasc@hotmail.com>
2022-05-01 21:11:35 -0300

    fix: inputValidation returning error messages

--- frontend/src/modules/inputValidation.js
+++ frontend/src/modules/inputValidation.js
@@ -1,49 +1,94 @@
 export function usernameValidate(username) {
-  const usernameRegex = /^[a-zA-Z0-9_]{6,20}$/; // 6-20 characters, letters, numbers, and underscores
-
-  return usernameRegex.test(username);
+  const regex = /^(?=.{6,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/; // 8-20 characters, no underscore, no double underscore
+  if (regex.test(username)) {
+    return {
+      isValid: true,
+      error: null,
+    };
+  } else {
+    return {
+      isValid: false,
+      error: 'Username must be between 8 and 20 characters, no underscore, no double underscore',
+    };
+  }
 }
 
 export function passwordValidate(password) {
   const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d\w\W]{8,}$/; // 8+ characters, at least one lowercase letter, one uppercase letter, one number, special characters allowed
-
-  return passwordRegex.test(password);
+  if (passwordRegex.test(password)) {
+    return {
+      isValid: true,
+      error: null,
+    };
+  } else {
+    return {
+      isValid: false,
+      error:
+        'Password must be at least 8 characters long and contain at least one lowercase letter, one uppercase letter, one number',
+    };
+  }
 }
 
 export function emailValidate(email) {
-  const emailRegex =
-    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/; // email address
-
-  return emailRegex.test(email);
+  const emailRegex = /^\w+([\.-]?\w+)+@\w+([\.:]?\w+)+(\.[a-zA-Z0-9]{2,4})+$/; // eslint-disable-line
+  if (emailRegex.test(email)) {
+    return {
+      isValid: true,
+      error: null,
+    };
+  } else {
+    return {
+      isValid: false,
+      error: 'Email address is not valid',
+    };
+  }
 }
 
-export function birthdayValidate(birthdate) {
+export function birthdateValidate(birthdate) {
   const birthdayRegex = /^\d{4}-\d{2}-\d{2}$/; // YYYY-MM-DD
-
-  return birthdayRegex.test(birthdate);
+  if (birthdayRegex.test(birthdate)) {
+    return {
+      isValid: true,
+      error: null,
+    };
+  } else {
+    return {
+      isValid: false,
+      error: 'Birthdate is not valid',
+    };
+  }
 }
 
-export default function registerValidate(username, password, email, birthdate) {
-  const usernameValidated = usernameValidate(username);
-  const passwordValidated = passwordValidate(password);
-  const emailValidated = emailValidate(email);
-  const birthdayValidated = birthdayValidate(birthdate);
-
-  if (!!usernameValidated) {
-    return { error: 'Username is invalid' };
+export function photoValidate(base64Photo) {
+  const photoRegex = /^data:image\/(png|jpg|jpeg);base64,/; // base64 photo
+  if (photoRegex.test(base64Photo)) {
+    return {
+      isValid: true,
+      error: null,
+    };
+  } else {
+    return {
+      isValid: false,
+      error: 'Photo is not valid',
+    };
   }
+}
 
-  if (!!passwordValidated) {
-    return { error: 'Password is invalid' };
-  }
-
-  if (!!emailValidated) {
-    return { error: 'Email is invalid' };
-  }
-
-  if (!!birthdayValidated) {
-    return { error: 'Birthday is invalid' };
-  }
-
-  return { isValid: true };
+export default function registerValidate(username, password, confirmPassword, email, birthdate) {
+  const usernameValidation = usernameValidate(username);
+  const passwordValidation = passwordValidate(password, confirmPassword);
+  const emailValidation = emailValidate(email);
+  const birthdateValidation = birthdateValidate(birthdate);
+  return {
+    isValid:
+      usernameValidation.isValid &&
+      passwordValidation.isValid &&
+      emailValidation.isValid &&
+      birthdateValidation.isValid,
+    error:
+      usernameValidation.error ||
+      passwordValidation.error ||
+      emailValidation.error ||
+      birthdateValidation.error,
+  };
 }
